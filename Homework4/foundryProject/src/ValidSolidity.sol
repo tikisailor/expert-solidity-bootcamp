842d6a477a96c19772ff3f3b169be27824744f6d
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-05-01 19:44:45 +0800

    add foundry project

---
+++ Homework4/foundryProject/src/ValidSolidity.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+contract MyCon {
+    function a(
+        function (
+            function (
+                function (
+                    function (
+                        function (
+                            function (
+                                function (
+                                    function (
+                                        function () external pure
+                                    ) external pure
+                                ) external pure
+                            ) external pure
+                        ) external pure
+                    ) external pure
+                ) external pure
+            ) external pure
+        ) external pure
+    ) external pure {}
+
+    function b (
+        function (
+            function (
+                function (
+                    function (
+                        function (
+                            function (
+                                function (
+                                    function () external pure
+                                ) external pure
+                            ) external pure
+                        ) external pure
+                    ) external pure
+                ) external pure
+            ) external pure
+        ) external pure
+    ) external pure {}
+}
